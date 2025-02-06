bd44bbfdee26fe3345a8553bd888b29dee5dc2aa
danielorozco <danor.dcor@gmail.com>
2025-02-06 18:08:56 -0500

    Fix rules

--- config/settings.py
+++ config/settings.py
@@ -87,9 +87,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.config(default=DATABASE_URL)
-    }
+    DATABASES = {"default": dj_database_url.config(default=DATABASE_URL)}
 else:
     DATABASES = {
         "default": {
