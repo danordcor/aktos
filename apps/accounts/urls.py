41fb5cd6d44e24cd21c5458122c31f43db17be97
danielorozco <danor.dcor@gmail.com>
2025-02-06 14:09:31 -0500

    Add Collection

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AccountListView, ImportCSVView
 
 urlpatterns = [
-    path("accounts/", AccountListView.as_view(), name="accounts-list"),
-    path("accounts/import/", ImportCSVView.as_view(), name="import-csv"),
+    path("", AccountListView.as_view(), name="accounts-list"),
+    path("import/", ImportCSVView.as_view(), name="import-csv"),
 ]
